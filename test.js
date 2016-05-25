e75392aaf8df90df83833ca4c3270673ad535a91
Arthur <arthurx91@gmail.com>
2016-05-25 18:07:14 +0000

    Initial commit

---
+++ test.js
@@ -0,0 +1,10 @@
+var http = require("http");
+
+var app = http.createServer(function (req, res) {
+    res.end(JSON.stringify(req.headers));
+
+});
+
+app.listen(1337);
+console.log("Server running at http://localhost:1337/");
+
